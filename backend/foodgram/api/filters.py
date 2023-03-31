bd76c1ef3b6dec9eb27d50de65e657edb50cd3d8
Анастасия Паневская <AnastasiaPanevskaya@yandex.by>
2023-04-01 00:58:11 +0300

    update filters

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -40,13 +40,13 @@ class RecipeFilter(FilterSet):
     def filter_is_favorited(self, queryset, name, value):
         user = self.request.user
         if value and not user.is_anonymous:
-            return queryset.filter(favorites__user=user)
+            return queryset.filter(favorite_recipe__user=user)
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
         user = self.request.user
         if value and not user.is_anonymous:
-            return queryset.filter(baskets__user=user)
+            return queryset.filter(basket__user=user)
         return queryset
 
     class Meta:
