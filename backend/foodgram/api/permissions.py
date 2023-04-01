b2368c606d761d3b9f1f0503cd2b2dbbc32e6328
Анастасия Паневская <AnastasiaPanevskaya@yandex.by>
2023-04-01 14:02:17 +0300

    up permission

--- backend/foodgram/api/permissions.py
+++ backend/foodgram/api/permissions.py
@@ -3,14 +3,11 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 class IsAuthorOrAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
-        return (request.method in SAFE_METHODS
-                or request.user.is_authenticated
-                or request.user.is_staff)
+        return (request.method in SAFE_METHODS or
+                request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
-        return (obj.author == request.user
-                or request.method in SAFE_METHODS
-                or request.user.is_staff
+        return (obj.author == request.user or request.method in SAFE_METHODS
                 )
 
 
