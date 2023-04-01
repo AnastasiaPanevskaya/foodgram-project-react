e3de7fd3c5c4e28951bd5380324c8d73f39a6459
Анастасия Паневская <AnastasiaPanevskaya@yandex.by>
2023-04-02 02:26:59 +0300

    update

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -201,7 +201,7 @@ class RecipeSerializer(serializers.ModelSerializer):
         RecipeIngredient.objects.bulk_create(
             RecipeIngredient(
                 recipe=recipe,
-                ingredient=ingredient.get('ingredient'),
+                ingredient_id=ingredient.get('ingredient'),
                 amount=ingredient.get('amount')
             ) for ingredient in ingredients)
         return recipe
@@ -215,7 +215,7 @@ class RecipeSerializer(serializers.ModelSerializer):
         RecipeIngredient.objects.bulk_create(
             RecipeIngredient(
                 recipe=instance,
-                ingredient=ingredient.get('ingredient'),
+                ingredient_id=ingredient.get('ingredient'),
                 amount=ingredient.get('amount')
             ) for ingredient in ingredients)
         instance.save()
